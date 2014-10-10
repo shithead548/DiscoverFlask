918fd078622d849bbe3305d55f254cfb22304b34
Stanley Wu <bleedtodry@hotmail.com>
2014-10-10 23:59:49 +0800

    dsds

--- OfficalFlask.py
+++ OfficalFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -12,6 +12,18 @@ def hello_world():
 def show(username):
     return 'User %s' % username
 
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+with app.test_request_context():
+    print url_for('hello_world')
+    print url_for('about')
 
 if __name__ == '__main__':
     app.run()
